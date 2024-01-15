2d39de3d03b3e842ccd5258b911eeadf9abc0ae5
ndidichenko <114722075+ndidichenko@users.noreply.github.com>
2024-01-15 17:09:58 +0100

    exception's update

--- FirstPlugin/FirstPlugin/Class1.cs
+++ FirstPlugin/FirstPlugin/Class1.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using Autodesk.Revit.Attributes;
 using System.IO;
 using System.Collections.Specialized;
+using System.Diagnostics;
 
 namespace FirstPlugin
 {
@@ -69,82 +70,109 @@ namespace FirstPlugin
 
             using(Transaction transaction = new Transaction(doc, "Create Grids"))
             {
-                transaction.Start();
-
-               
-                //creating horizontal grids
-                Grid[] horizontalGrids = new Grid[10];
-                for (int i = 0; i < horizontalGrids.Length; i++)
+                try
                 {
-                   
-                    XYZ startPointHorizontal = new XYZ(50, (i-5)*10, 0);
-                    XYZ endPointHorizontal = new XYZ(-50, (i-5)*10, 0);
+                    transaction.Start();
+
+
+                    //creating horizontal grids
+                    Grid[] horizontalGrids = new Grid[10];
+                    for (int i = 0; i < horizontalGrids.Length; i++)
+                    {
+
+                        XYZ startPointHorizontal = new XYZ(50, (i - 5) * 10, 0);
+                        XYZ endPointHorizontal = new XYZ(-50, (i - 5) * 10, 0);
+
+                        Line horizontalLine = Line.CreateBound(startPointHorizontal, endPointHorizontal);
+                        string gridName = ((char)('a' + i)).ToString();
+                        if (GridNameExists(doc, gridName))
+                        {
+                            // handling the case with already created grids
+                            TaskDialog.Show("Error", $"Grids were already created.");
+                            return Result.Failed;
+                        }
+                        horizontalGrids[i] = Grid.Create(doc, horizontalLine);
+                    }
+
+
+                    //setting parameters of horizontal grids
+                    Parameter[] horizontalGridParameters = new Parameter[horizontalGrids.Length];
+                    for (int i = 0; i < horizontalGrids.Length; i++)
+                    {
+                        horizontalGridParameters[i] = horizontalGrids[i].get_Parameter(BuiltInParameter.DATUM_TEXT);
+                        if (GridNameExists(doc, ('a' + i).ToString()))
+                        {
+                            // handling the case with already created grids
+
+                            TaskDialog.Show("Error", $"Grids were already created.");
+                            return Result.Failed;
+                        }
+                        horizontalGridParameters[i].Set(((char)('a' + i)).ToString());
+                    }
+
+
+                    //creating vertical grids
+                    Grid[] verticalGrids = new Grid[10];
+                    for (int i = 0; i < verticalGrids.Length; i++)
+                    {
+
+                        XYZ startPointVertical = new XYZ((i - 5) * 10, 50, 0);
+                        XYZ endPointVertical = new XYZ((i - 5) * 10, -50, 0);
+
+                        Line verticalLine = Line.CreateBound(startPointVertical, endPointVertical);
+
+                        string gridName = (i + 1).ToString();
+                        if (GridNameExists(doc, gridName))
+                        {
+                            // handling the case with already created grids
+
+                            TaskDialog.Show("Error", $"Grids were already created.");
+                            return Result.Failed;
+                        }
+                        verticalGrids[i] = Grid.Create(doc, verticalLine);
+                    }
+
+
+                    //setting vertical grids' parameters
+                    Parameter[] verticalGridParameters = new Parameter[verticalGrids.Length];
+                    for (int i = 0; i < verticalGrids.Length; i++)
+                    {
+                        verticalGridParameters[i] = verticalGrids[i].get_Parameter(BuiltInParameter.DATUM_TEXT);
+                        if (GridNameExists(doc, (i + 1).ToString()))
+                        {
+                            // handling the case with already created grids
+
+                            TaskDialog.Show("Error", $"Grids were already created.");
+                            return Result.Failed;
+                        }
+                        verticalGridParameters[i].Set((i + 1).ToString());
+
+                    }
+
+                    transaction.Commit();
+
+                }
+                catch (Exception ex)
+                {
+                    //show dialog if error
+                    //TaskDialog.Show("Error", "Error: " + ex.Message);
+                    TaskDialog td = new TaskDialog("Error");
                     
-                    Line horizontalLine = Line.CreateBound(startPointHorizontal, endPointHorizontal);
-                    string gridName = ((char)('a' + i)).ToString();
-                    if (GridNameExists(doc, gridName))
-                    {
-                        // handling the case with already created grids
-                        TaskDialog.Show("Error", $"Grids were already created.");
-                        return Result.Failed; 
-                    }
-                    horizontalGrids[i] = Grid.Create(doc, horizontalLine);
-                }
+                    td.Title = "Error";
+                    td.AllowCancellation = true;
+                    
+                    // message related
 
+                    td.MainInstruction = "Error";
+                    td.MainContent = "Error: " + ex.Message;
+                    td.CommonButtons = TaskDialogCommonButtons.Close;
 
-                //setting parameters of horizontal grids
-                Parameter[] horizontalGridParameters = new Parameter[horizontalGrids.Length];
-                for (int i = 0; i < horizontalGrids.Length; i++)
-                {
-                    horizontalGridParameters[i] = horizontalGrids[i].get_Parameter(BuiltInParameter.DATUM_TEXT);
-                    if(GridNameExists(doc, ('a' + i).ToString())){
-                        // handling the case with already created grids
+                    td.Show();
 
-                        TaskDialog.Show("Error", $"Grids were already created.");
-                        return Result.Failed;
-                    }
-                        horizontalGridParameters[i].Set(((char)('a'+i)).ToString());
-                }
-
-
-                //creating vertical grids
-                Grid[] verticalGrids = new Grid[10];
-                for (int i = 0; i < verticalGrids.Length; i++)
-                {
-
-                    XYZ startPointVertical = new XYZ((i - 5) * 10, 50, 0);
-                    XYZ endPointVertical = new XYZ((i - 5) * 10, -50, 0);
-
-                    Line verticalLine = Line.CreateBound(startPointVertical, endPointVertical);
-
-                    string gridName = (i + 1).ToString();
-                    if (GridNameExists(doc, gridName))
-                    {
-                        // handling the case with already created grids
-
-                        TaskDialog.Show("Error", $"Grids were already created.");
-                        return Result.Failed;
-                    }
-                    verticalGrids[i] = Grid.Create(doc, verticalLine);
-                }
-
-
-                //setting vertical grids' parameters
-                Parameter[] verticalGridParameters = new Parameter[verticalGrids.Length];
-                for(int i = 0; i < verticalGrids.Length; i++)
-                {
-                   verticalGridParameters[i] = verticalGrids[i].get_Parameter(BuiltInParameter.DATUM_TEXT);
-                    if (GridNameExists(doc, (i + 1).ToString())){
-                        // handling the case with already created grids
-
-                        TaskDialog.Show("Error", $"Grids were already created.");
-                        return Result.Failed;
-                    }
-                        verticalGridParameters[i].Set((i +1).ToString());
+                    Debug.Print(ex.Message);
+                    transaction.RollBack();
 
                 }
-
-                transaction.Commit();
 
                 return Result.Succeeded;
 
